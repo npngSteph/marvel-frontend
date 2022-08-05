16ef15caa8f4d4ef5a02244d6f638802c7247234
npngSteph <stephane.ahmedboulanger@gmail.com>
2022-08-05 23:19:52 +0200

    add comics

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect, startTransition } from "react";
+import axios from "axios";
 
 function App() {
+  const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchMyMarvelBackend = async () => {
+      const response = await axios.get("http://localhost:4000/characters");
+      setData(response.data);
+      setIsLoading(false);
+    };
+
+    fetchMyMarvelBackend();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/Marvel_Logo.svg/2560px-Marvel_Logo.svg.png"
+        width={100}
+        height={50}
+      />
+
+      <nav className="menu">
+        <ul className="container">
+          <li className="button">Characters</li>
+          <li className="button">Comics</li>
+          <li className="button">Favorites</li>
+        </ul>
+      </nav>
+
+      {isLoading === true ? (
+        <h2>En cours de chargement</h2>
+      ) : (
+        <>
+          <p>test</p>
+          {console.log(data)}
+          {data.results.map((item) => {
+            return (
+              <div>
+                <img src={item.thumbnail.path} />
+                <h1>{item.name}</h1>
+                <h6>{item.description}</h6>
+              </div>
+            );
+          })}
+        </>
+      )}
     </div>
   );
 }
